c53bbb9bf3e05bb1238bddc9e8176dc029666a14
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-12-24 02:05:08 +0300

    init

---
+++ svelte.config.js
@@ -0,0 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess({ script: true }),
+
+	// reference: https://svelte.dev/docs/kit/adapter-static#GitHub-Pages
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
