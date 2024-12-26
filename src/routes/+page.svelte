703ca1e19a912ac9e355d6de629d562e9ab4a472
pier-bezuhoff <pierbezuhoff2016@gmail.com>
2024-12-26 16:14:37 +0300

    style cleanup + github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,9 +93,9 @@
       context.clearRect(0, 0, w, h);
       context.beginPath();
       context.moveTo(0, 0);
-      context.lineTo(w - 1, 0);
-      context.lineTo(w - 1, h - 1);
-      context.lineTo(0, h - 1);
+      context.lineTo(w, 0);
+      context.lineTo(w, h);
+      context.lineTo(0, h);
       context.closePath();
       context.stroke();
       if (vertices != null && vertices.length > 0) {
@@ -172,7 +172,7 @@
     name="description"
     content="Calculate area of any polygon by its sides and some angles with picture | Free Online Calculator &amp; Vizualizer for Arbitrary Polygons"
   />
-  <!-- 
+  <!-- Noto Sans Math seems to be pre-installed, so no need to import it
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
@@ -182,10 +182,22 @@
   -->
   <meta name="google-site-verification" content="yKOpXb8Zc0SJds3eFMCCXDqQp52QWU2oVZR0exhXWxU" />
 </svelte:head>
+<header>
+  <div class="top-right-icon-row">
+    <a
+      href="https://github.com/pier-bezuhoff/polygon-area"
+      target="_blank"
+      rel="noopener noreferrer"
+      data-icon="github"
+      aria-label="GitHub Repo"
+      class="github-link {lightScheme ? 'github-link-light' : 'github-link-dark'}"
+    ></a>
+    <button type="button" class="light-dark-toggle" onclick={toggleColorScheme}>
+      {@html lightScheme ? '&#9728;' : '&#127768;'}
+    </button>
+  </div>
+</header>
 <main>
-  <button type="button" class="light-dark-toggle" onclick={toggleColorScheme}
-    >{@html lightScheme ? '&#9728;' : '&#127768;'}</button
-  >
   <div class="row">
     <div class="left-column">
       <div class="calcs">
@@ -215,17 +227,20 @@
           {/each}
         </div>
         <div class="buttons-row">
-          <button type="button" class="control-button" onclick={addSide}>Add another side</button>
-          <button type="button" class="control-button" onclick={deleteSide}>Delete last side</button
-          >
-          <button type="button" class="control-button" onclick={toRegularPolygon}
-            >Regular polygon</button
-          >
+          <button type="button" class="control-button" onclick={addSide}> Add another side </button>
+          <button type="button" class="control-button" onclick={deleteSide}>
+            Delete last side
+          </button>
+          <button type="button" class="control-button" onclick={toRegularPolygon}>
+            Regular polygon
+          </button>
         </div>
         {#if area != null}
           <hr class="result-separator" />
           <div class="result-wrap">
-            <h2>Area <i>S</i> = <i class="actual-area">{area.toPrecision(6)}</i></h2>
+            <h2 id="answer">
+              Area <i>S</i> = <i class="actual-area">{area.toPrecision(6)}</i>
+            </h2>
           </div>
         {/if}
       </div>
@@ -247,47 +262,83 @@
     --accent-color-dark: hsl(280, 100%, 70%);
     --bg-color-light: white;
     --bg-color-dark: rgb(31, 31, 31); /* recommmended bg for dark mode */
+    --button-bg-color-light: hsl(0, 0%, 90%);
+    --button-bg-color-dark: hsl(0, 0%, 30%);
+    --button-hover-bg-color-light: hsl(0, 0%, 80%);
+    --button-hover-bg-color-dark: hsl(0, 0%, 40%);
+    --button-pressed-bg-color-light: hsl(0, 0%, 70%);
+    --button-pressed-bg-color-dark: hsl(0, 0%, 50%);
     --text-color: light-dark(black, white);
     --bg-color: light-dark(var(--bg-color-light), var(--bg-color-dark));
     --accent-color: light-dark(var(--accent-color-light), var(--accent-color-dark));
+    --button-bg-color: light-dark(var(--button-bg-color-light), var(--button-bg-color-dark));
+    --button-hover-bg-color: light-dark(
+      var(--button-hover-bg-color-light),
+      var(--button-hover-bg-color-dark)
+    );
+    --button-pressed-bg-color: light-dark(
+      var(--button-pressed-bg-color-light),
+      var(--button-pressed-bg-color-dark)
+    );
     color: var(--text-color);
     background-color: var(--bg-color);
   }
-  /* Hide arrows on number inputs */
-  /* Chrome, Safari, Edge, Opera */
-  :global(input::-webkit-outer-spin-button),
-  :global(input::-webkit-inner-spin-button) {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  /* Firefox */
-  :global(input[type='number']) {
-    -moz-appearance: textfield;
+
+  .top-right-icon-row {
+    display: flex;
+    flex-direction: row;
+    justify-content: end;
+    padding: 8px;
+    margin-right: 16px;
+    gap: 12px;
   }
 
-  main,
-  input[type='number'] {
-    font-family: 'Noto Sans Math';
-    font-size: 18px;
+  button {
+    cursor: pointer;
+  }
+
+  .github-link {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background-size: contain; /* Ensures the SVG fits within the container while maintaining its aspect ratio */
+    text-decoration: none;
+  }
+
+  .github-link:hover {
+    filter: brightness(150%);
+  }
+
+  .github-link-light {
+    background-image: url("data:image/svg+xml,%3csvg%20width='98'%20height='96'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20fill-rule='evenodd'%20clip-rule='evenodd'%20d='M48.854%200C21.839%200%200%2022%200%2049.217c0%2021.756%2013.993%2040.172%2033.405%2046.69%202.427.49%203.316-1.059%203.316-2.362%200-1.141-.08-5.052-.08-9.127-13.59%202.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015%204.934.326%207.523%205.052%207.523%205.052%204.367%207.496%2011.404%205.378%2014.235%204.074.404-3.178%201.699-5.378%203.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283%200-5.378%201.94-9.778%205.014-13.2-.485-1.222-2.184-6.275.486-13.038%200%200%204.125-1.304%2013.426%205.052a46.97%2046.97%200%200%201%2012.214-1.63c4.125%200%208.33.571%2012.213%201.63%209.302-6.356%2013.427-5.052%2013.427-5.052%202.67%206.763.97%2011.816.485%2013.038%203.155%203.422%205.015%207.822%205.015%2013.2%200%2018.905-11.404%2023.06-22.324%2024.283%201.78%201.548%203.316%204.481%203.316%209.126%200%206.6-.08%2011.897-.08%2013.526%200%201.304.89%202.853%203.316%202.364%2019.412-6.52%2033.405-24.935%2033.405-46.691C97.707%2022%2075.788%200%2048.854%200z'%20fill='%23666'/%3e%3c/svg%3e");
+  }
+
+  .github-link-dark {
+    background-image: url("data:image/svg+xml,%3csvg%20width='98'%20height='96'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20fill-rule='evenodd'%20clip-rule='evenodd'%20d='M48.854%200C21.839%200%200%2022%200%2049.217c0%2021.756%2013.993%2040.172%2033.405%2046.69%202.427.49%203.316-1.059%203.316-2.362%200-1.141-.08-5.052-.08-9.127-13.59%202.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015%204.934.326%207.523%205.052%207.523%205.052%204.367%207.496%2011.404%205.378%2014.235%204.074.404-3.178%201.699-5.378%203.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283%200-5.378%201.94-9.778%205.014-13.2-.485-1.222-2.184-6.275.486-13.038%200%200%204.125-1.304%2013.426%205.052a46.97%2046.97%200%200%201%2012.214-1.63c4.125%200%208.33.571%2012.213%201.63%209.302-6.356%2013.427-5.052%2013.427-5.052%202.67%206.763.97%2011.816.485%2013.038%203.155%203.422%205.015%207.822%205.015%2013.2%200%2018.905-11.404%2023.06-22.324%2024.283%201.78%201.548%203.316%204.481%203.316%209.126%200%206.6-.08%2011.897-.08%2013.526%200%201.304.89%202.853%203.316%202.364%2019.412-6.52%2033.405-24.935%2033.405-46.691C97.707%2022%2075.788%200%2048.854%200z'%20fill='%23d4d4d4'/%3e%3c/svg%3e");
+  }
+
+  .light-dark-toggle {
+    position: relative;
+    top: 0px;
+    float: right;
+    font-size: 30px;
+    background-color: transparent;
+    border: none;
+  }
+
+  .light-dark-toggle:hover {
+    filter: brightness(150%);
   }
 
   main {
-    margin: 24px;
-  }
-
-  input[type='number'] {
-    width: 60px;
+    font-family: 'Noto Sans Math';
+    font-size: 18px;
   }
 
   .row {
     display: flex;
     flex-direction: row;
     justify-content: center;
-  }
-
-  .left-column,
-  .right-column {
-    padding: 12px;
   }
 
   .left-column {
@@ -298,20 +349,9 @@
     justify-content: top;
   }
 
-  .right-column {
-    width: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
-
   .calcs {
     padding-left: 24px;
     padding-right: 24px;
-  }
-
-  .entries {
-    /* margin-left: 8px; */
   }
 
   .entry-row {
@@ -336,6 +376,27 @@
     margin-left: 16px;
   }
 
+  /* Hide arrows on number inputs */
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  /* Firefox */
+  input[type='number'] {
+    -moz-appearance: textfield;
+  }
+
+  input[type='number'] {
+    font-family: 'Noto Sans Math';
+    font-size: 18px;
+  }
+
+  input[type='number'] {
+    width: 60px;
+  }
+
   .side-input {
     color: var(--accent-color);
   }
@@ -350,6 +411,18 @@
   .control-button {
     margin-right: 16px;
     font-size: 16px;
+    background-color: var(--button-bg-color);
+    border: solid;
+    border-radius: 4px;
+    border-width: 1px;
+  }
+
+  .control-button:hover {
+    background-color: var(--button-hover-bg-color);
+  }
+
+  .control-button:active {
+    background-color: var(--button-pressed-bg-color);
   }
 
   .result-separator {
@@ -370,27 +443,31 @@
     padding-right: 12px;
   }
 
+  .right-column {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+
   .canvas-wrap {
     display: flex;
     flex-direction: row;
     justify-content: start;
-    padding: 48px;
+    padding-left: 48px;
+    margin-top: 36px;
   }
 
   canvas {
-    width: 90%;
+    width: 80%;
   }
 
-  .light-dark-toggle {
-    position: relative;
-    top: 0px;
-    float: right;
-    font-size: 30px;
-    background-color: transparent;
-    border: none;
-  }
-
-  .light-dark-toggle:hover {
-    filter: brightness(150%);
+  .unselectable {
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
   }
 </style>
